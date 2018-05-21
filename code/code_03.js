d1287e5987dd712f2fc72802c586da7b84126328
hilbert <1056377688@qq.com>
2018-05-21 14:35:34 +0800

    q3 code

--- code/code_03.js
+++ code/code_03.js
@@ -12,10 +12,10 @@ Dictionary.prototype.findMostSimilar = function (term) {
         let distance = [];
         distance[0] = [];
         // 构造从0开始的二维数组
-        for (let i = 0; i <= aLength; i++) {
+        for (let i = 0; i <= bLength; i++) {
             distance[0].push(i);
         }
-        for (let j = 0; j <= bLength; j++) {
+        for (let j = 0; j <= aLength; j++) {
             if (distance[j]) {
                 distance[j][0] = j;
             } else {
@@ -23,20 +23,24 @@ Dictionary.prototype.findMostSimilar = function (term) {
                 distance[j][0] = j;
             }
         }
-        for (let i = 1; i < aLength; i++) {
-            for (let j = 1; j < bLength; j++) {
+        for (let i = 1; i <= aLength; i++) {
+            for (let j = 1; j <= bLength; j++) {
                 if (a[i - 1] === b[j - 1]) {
                     distance[i][j] = distance[i - 1][j - 1];
                 } else {
-                    // 周末完成
+                    let add = distance[i - 1][j] + 1;
+                    let del = distance[i][j - 1] + 1;
+                    let replace = distance[i - 1][j - 1] + 1;
+                    distance[i][j] = Math.min(add, del, replace);
                 }
             }
         }
-        console.log(distance);
+        return distance[aLength][bLength];
     }
     for (let i = 0; i < this.words.length; i++) {
-        leastStep(this.words[i], term)
+        steps.push(leastStep(this.words[i], term));
     }
+    return this.words[steps.indexOf(Math.min(...steps))];
 }
 
 const test = new Dictionary(['cherry', 'pineapple', 'melon', 'strawberry', 'raspberry']);
