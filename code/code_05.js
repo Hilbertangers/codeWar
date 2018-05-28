50c9b441882324c582a6c578de94f56e6105d5f4
hilbert <1056377688@qq.com>
2018-05-28 20:20:38 +0800

    q5 code for home

--- code/code_05.js
+++ code/code_05.js
@@ -1,3 +1,23 @@
+// 原理C(m, n) = C(m - 1, n - 1) + C(m - 1, n);
+// 当 m = n || m = 0 || n = 0 返回 1;
 function pascalsTriangle(n) {
-    //return a flat array representing the values of Pascal's Triangle to the n-th level
+    if (n < 1) return;
+    let arr = [];
+    let getPoint = (m, n) => {
+        if (m === n || m === 0 || n === 0) {
+            return 1;
+        } else {
+            return getPoint(m - 1, n - 1) + getPoint(m - 1, n);
+        }
+    }
+    // error
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - 1; j++) {
+            console.log(i, j);
+            arr.push(getPoint(j, i));
+        }
+    }
+    console.log(arr);
+    return arr;
 }
+pascalsTriangle(4);
