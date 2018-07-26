00f006b431b2d21dca7d185ed8975315c18b27fa
hilbert <1056377688@qq.com>
2018-07-26 14:16:58 +0800

    code8 abandon

--- code/code_08.js
+++ code/code_08.js
@@ -1,22 +1,56 @@
+// 未完成 1、没有去重 2、没有处理除法 3、没有去除重复括号
 function equalTo24(a, b, c, d) {
-	let nums = [a, b, c, d];
+	let nums = [{
+		value: a,
+		type: a
+	},{
+		value: b,
+		type: b
+	},{
+		value: c,
+		type: c
+	},{
+		value: d,
+		type: d
+	}];
+	let steps = '';
+
+
 	function kar98k(arr) {
 		if (arr.length >= 2) {
 			for (let i = 0; i < arr.length; i++) {
 				for (let j = i + 1; j < arr.length; j++) {
 					let results = [];
 					// 加法
-					results.push(arr[i] + arr[j]);
+					results.push({
+						value: arr[i]['value'] + arr[j]['value'],
+						type: `(${arr[i]['type']}+${arr[j]['type']})`
+					});
 					// 减法
-					results.push(arr[i] - arr[j]);
+					results.push({
+						value: arr[i]['value'] - arr[j]['value'],
+						type: `(${arr[i]['type']}-${arr[j]['type']})`
+					});
 					// 乘法
-					results.push(arr[i] * arr[j]);
+					results.push({
+						value: arr[i]['value'] * arr[j]['value'],
+						type: `${arr[i]['type']}*${arr[j]['type']}`
+					});
 					// 除法
-					// results.push(arr[i] + '/' + arr[j]);
+					results.push({
+						value: eval(`${arr[i]['value']}/${arr[j]['value']}`),
+						type: `(${arr[i]['type']}/${arr[j]['type']})`
+					});
 					// 反减
-					results.push(arr[j] - arr[i]);
+					results.push({
+						value: arr[j]['value'] - arr[i]['value'],
+						type: `(${arr[j]['type']}-${arr[i]['type']})`
+					});
 					// 反除
-					// results.push(arr[j] + '/' + arr[i]);
+					results.push({
+						value: eval(`${arr[j]['value']}/${arr[i]['value']}`),
+						type: `(${arr[i]['type']}/${arr[j]['type']})`
+					});
 					if (arr.length > 2) {
 						// delet i, j
 						let params = [];
@@ -29,18 +63,17 @@ function equalTo24(a, b, c, d) {
 							kar98k([item, ...params]);
 						});
 					} else {
-						console.log('comp', results);
 						return results.some(item => {
-							if (item === 24) {
+							if (item.value === 24) {
 								return true;
 							}
-						}) && 'ok';
+						});
 					}
 				}
 			}
 		}
 	}
-	console.log(kar98k(nums));
+	kar98k(nums);
 }
 
-equalTo24(6,6,6,6);
+equalTo24(5,5,10,1);
